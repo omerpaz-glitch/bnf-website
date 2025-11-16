cf31b9d98b11b663b74cad4e87ef34265579a02b
omerpaz-glitch <omer.paz@planet-nine.io>
2025-11-16 18:06:06 +0200

    Implement smooth scrolling for hash links
    Added smooth scrolling for hash links and reset scroll position for no hash.

--- bnfwebsite-main/src/pages/Index.tsx
+++ bnfwebsite-main/src/pages/Index.tsx
@@ -1,3 +1,6 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
 import AnimatedBackground from "@/components/AnimatedBackground";
 import Hero from "@/components/Hero";
 import AboutSection from "@/components/AboutSection";
@@ -9,26 +12,45 @@ import Footer from "@/components/Footer";
 import TopNav from "@/components/TopNav";
 
 const Index = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    // If URL contains a hash like #contact, #what-we-do, etc
+    if (location.hash) {
+      const id = location.hash.replace("#", "");
+      const el = document.getElementById(id);
+
+      if (el) {
+        el.scrollIntoView({ behavior: "smooth" });
+      }
+    } else {
+      // No hash: start at the top
+      window.scrollTo(0, 0);
+    }
+  }, [location]);
+
   return (
     <div className="relative min-h-screen bg-background overflow-hidden">
       <AnimatedBackground />
       <TopNav />
-      
+
       <main className="relative z-10">
         <Hero />
         <AboutSection />
+
         <div id="what-we-do">
           <WhatWeDoSection />
         </div>
+
         <CompanyCards />
+
         <div id="penetration-testing">
           <PenetrationTestingSection />
         </div>
+
         <ContactForm />
         <Footer />
       </main>
     </div>
   );
 };
-
-export default Index;
